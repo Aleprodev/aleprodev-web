51cd1414af2ca4bb30351768bdef84c42490430c
Alejandro Piña <aleprodev@gmail.com>
2025-04-07 04:04:26 -0300

    [fix] gh-pages settings

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true, // necesario para export estático
+  },
+  basePath: "/aleprodev-web",
+  assetPrefix: "/aleprodev-web/",
 };
 
 export default nextConfig;
