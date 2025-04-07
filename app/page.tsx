78ea6b31a2adf348b98ad6d95fbbef022b66f4c1
Alejandro Piña <aleprodev@gmail.com>
2025-04-07 04:35:25 -0300

    [fix] gh-pages settings

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
-import Hobbies from '@/components/Hobbies'
-import Hero from '@/components/Hero'
-import Footer from '@/components/layout/Footer'
-import Projects from '@/components/Projects'
-import EmailMe from './components/shared/EmailMe'
+"use client";
+import Hobbies from "@/components/Hobbies";
+import Hero from "@/components/Hero";
+import Footer from "@/components/layout/Footer";
+import Projects from "@/components/Projects";
+import EmailMe from "./components/shared/EmailMe";
 
 export default function Home() {
   return (
@@ -13,6 +14,5 @@ export default function Home() {
       <EmailMe />
       <Footer />
     </>
-  )
+  );
 }
-
